2c1a97f6f1f4c778803bc297bf9a156a00f98b8c
lemmav <valeriya.bagisheva@onlyoffice.com>
2023-07-17 15:06:48 +0300

    All to def

--- build/install/docker/docker-entrypoint.py
+++ build/install/docker/docker-entrypoint.py
@@ -28,8 +28,8 @@ APP_CORE_MACHINEKEY = os.environ["APP_CORE_MACHINEKEY"] if environ.get("APP_CORE
 INSTALLATION_TYPE = os.environ["INSTALLATION_TYPE"].upper() if environ.get("INSTALLATION_TYPE") else "ENTERPRISE"
 APP_URL_PORTAL = os.environ["APP_URL_PORTAL"] if environ.get("APP_URL_PORTAL") else "http://" + ROUTER_HOST + ":8092"
 APP_STORAGE_ROOT = os.environ["APP_STORAGE_ROOT"] if environ.get("APP_STORAGE_ROOT") else BASE_DIR + "/data/"
-APP_KNOWN_PROXIES = os.environ["APP_KNOWN_PROXIES"]
-APP_KNOWN_NETWORKS = os.environ["APP_KNOWN_NETWORKS"]
+APP_KNOWN_PROXIES = os.environ["APP_KNOWN_PROXIES"] if environ.get("APP_KNOWN_PROXIES") is not None else "42"
+APP_KNOWN_NETWORKS = os.environ["APP_KNOWN_NETWORKS"] if environ.get("APP_KNOWN_NETWORKS") is not None else "42"
 
 DOCUMENT_SERVER_JWT_SECRET = os.environ["DOCUMENT_SERVER_JWT_SECRET"] if environ.get("DOCUMENT_SERVER_JWT_SECRET") else "your_jwt_secret"
 DOCUMENT_SERVER_JWT_HEADER = os.environ["DOCUMENT_SERVER_JWT_HEADER"] if environ.get("DOCUMENT_SERVER_JWT_HEADER") else "AuthorizationJwt"
@@ -58,172 +58,186 @@ RABBIT_PORT =  os.environ["RABBIT_PORT"] if environ.get("RABBIT_PORT") else "567
 RABBIT_VIRTUAL_HOST = os.environ["RABBIT_VIRTUAL_HOST"] if environ.get("RABBIT_VIRTUAL_HOST") else "/"
 RABBIT_URI = {"Uri": os.environ["RABBIT_URI"]} if environ.get("RABBIT_URI") else None
 
-class RunServices:
-    def __init__(self, SERVICE_PORT, PATH_TO_CONF):
-        self.SERVICE_PORT = SERVICE_PORT
-        self.PATH_TO_CONF = PATH_TO_CONF
-    @dispatch(str)    
-    def RunService(self, RUN_FILE):
-        os.system("node " + RUN_FILE + " --app.port=" + self.SERVICE_PORT +\
-             " --app.appsettings=" + self.PATH_TO_CONF)
-        return 1
-        
-    @dispatch(str, str)
-    def RunService(self, RUN_FILE, ENV_EXTENSION):
-        if ENV_EXTENSION == "none":
-            self.RunService(RUN_FILE)
-        os.system("node " + RUN_FILE + " --app.port=" + self.SERVICE_PORT +\
-             " --app.appsettings=" + self.PATH_TO_CONF +\
-                " --app.environment=" + ENV_EXTENSION)
-        return 1
+def main():
+    class RunServices:
+        def __init__(self, SERVICE_PORT, PATH_TO_CONF):
+            self.SERVICE_PORT = SERVICE_PORT
+            self.PATH_TO_CONF = PATH_TO_CONF
+        @dispatch(str)    
+        def RunService(self, RUN_FILE):
+            os.system("node " + RUN_FILE + " --app.port=" + self.SERVICE_PORT +\
+                " --app.appsettings=" + self.PATH_TO_CONF)
+            return 1
+            
+        @dispatch(str, str)
+        def RunService(self, RUN_FILE, ENV_EXTENSION):
+            if ENV_EXTENSION == "none":
+                self.RunService(RUN_FILE)
+            os.system("node " + RUN_FILE + " --app.port=" + self.SERVICE_PORT +\
+                " --app.appsettings=" + self.PATH_TO_CONF +\
+                    " --app.environment=" + ENV_EXTENSION)
+            return 1
 
-    @dispatch(str, str, str)
-    def RunService(self, RUN_FILE, ENV_EXTENSION, LOG_FILE):
-        data = RUN_FILE.split(".")
-        if data[-1] != "dll":
-            self.RunService(RUN_FILE, ENV_EXTENSION)
-        elif  ENV_EXTENSION == "none":
-            os.system("dotnet " + RUN_FILE + " --urls=" + URLS + self.SERVICE_PORT +\
-                " --\'$STORAGE_ROOT\'=" + APP_STORAGE_ROOT +\
-                    " --pathToConf=" + self.PATH_TO_CONF +\
-                        " --log:dir=" + LOG_DIR +\
-                            " --log:name=" + LOG_FILE +\
-                                " core:products:folder=/var/www/products/" +\
-                                    " core:products:subfolder=server" + " " +\
-                                        CORE_EVENT_BUS)
-        else:
-            os.system("dotnet " + RUN_FILE + " --urls=" + URLS + self.SERVICE_PORT +\
-                 " --\'$STORAGE_ROOT\'=" + APP_STORAGE_ROOT +\
-                    " --pathToConf=" + self.PATH_TO_CONF +\
-                        " --log:dir=" + LOG_DIR +\
-                            " --log:name=" + LOG_FILE +\
-                                " --ENVIRONMENT=" + ENV_EXTENSION +\
+        @dispatch(str, str, str)
+        def RunService(self, RUN_FILE, ENV_EXTENSION, LOG_FILE):
+            data = RUN_FILE.split(".")
+            if data[-1] != "dll":
+                self.RunService(RUN_FILE, ENV_EXTENSION)
+            elif  ENV_EXTENSION == "none":
+                os.system("dotnet " + RUN_FILE + " --urls=" + URLS + self.SERVICE_PORT +\
+                    " --\'$STORAGE_ROOT\'=" + APP_STORAGE_ROOT +\
+                        " --pathToConf=" + self.PATH_TO_CONF +\
+                            " --log:dir=" + LOG_DIR +\
+                                " --log:name=" + LOG_FILE +\
                                     " core:products:folder=/var/www/products/" +\
                                         " core:products:subfolder=server" + " " +\
                                             CORE_EVENT_BUS)
+            else:
+                os.system("dotnet " + RUN_FILE + " --urls=" + URLS + self.SERVICE_PORT +\
+                    " --\'$STORAGE_ROOT\'=" + APP_STORAGE_ROOT +\
+                        " --pathToConf=" + self.PATH_TO_CONF +\
+                            " --log:dir=" + LOG_DIR +\
+                                " --log:name=" + LOG_FILE +\
+                                    " --ENVIRONMENT=" + ENV_EXTENSION +\
+                                        " core:products:folder=/var/www/products/" +\
+                                            " core:products:subfolder=server" + " " +\
+                                                CORE_EVENT_BUS)
 
-def openJsonFile(filePath):
-    try:
-        with open(filePath, 'r') as f:
-            return json.load(f)
-    except FileNotFoundError as e:
-        return False
-    except IOError as e:
-        return False
+    def openJsonFile(filePath):
+        try:
+            with open(filePath, 'r') as f:
+                return json.load(f)
+        except FileNotFoundError as e:
+            return False
+        except IOError as e:
+            return False
 
-def parseJsonValue(jsonValue):
-    data = jsonValue.split("=")
-    data[0] = "$." + data[0].strip()
-    data[1] = data[1].replace(" ", "")
-    
-    return data
+    def parseJsonValue(jsonValue):
+        data = jsonValue.split("=")
+        data[0] = "$." + data[0].strip()
+        data[1] = data[1].replace(" ", "")
+        
+        return data
 
-def updateJsonData(jsonData, jsonKey, jsonUpdateValue):
-    jsonpath_expr = parse(jsonKey)
-    jsonpath_expr.find(jsonData)
-    jsonpath_expr.update(jsonData, jsonUpdateValue)
-    
-    return jsonData
+    def updateJsonData(jsonData, jsonKey, jsonUpdateValue):
+        jsonpath_expr = parse(jsonKey)
+        jsonpath_expr.find(jsonData)
+        jsonpath_expr.update(jsonData, jsonUpdateValue)
+        
+        return jsonData
 
-def writeJsonFile(jsonFile, jsonData, indent=4):
-    with open(jsonFile, 'w') as f:
-        f.write(json.dumps(jsonData, ensure_ascii=False, indent=indent))
-    
-    return 1
+    def writeJsonFile(jsonFile, jsonData, indent=4):
+        with open(jsonFile, 'w') as f:
+            f.write(json.dumps(jsonData, ensure_ascii=False, indent=indent))
+        
+        return 1
 
-#filePath = sys.argv[1]
-saveFilePath = filePath
-#jsonValue = sys.argv[2]
+    #filePath = sys.argv[1]
+    saveFilePath = filePath
+    #jsonValue = sys.argv[2]
 
-filePath = "/app/onlyoffice/config/appsettings.json"
-jsonData = openJsonFile(filePath)
-#jsonUpdateValue = parseJsonValue(jsonValue)
+    filePath = "/app/onlyoffice/config/appsettings.json"
+    jsonData = openJsonFile(filePath)
+    #jsonUpdateValue = parseJsonValue(jsonValue)
 
 
-parametrsForUpdate = {
-"$.ConnectionStrings.default.connectionString": "Server="+MYSQL_HOST+";Port=3306;Database="+MYSQL_DATABASE+";User ID="+MYSQL_USER+";Password="+MYSQL_PASSWORD+";Pooling=true;Character Set=utf8;AutoEnlist=false;SSL Mode=none;ConnectionReset=false",
-"$.core.base-domain": APP_CORE_BASE_DOMAIN,
-"$.core.machinekey": APP_CORE_MACHINEKEY,
-"$.core.products.subfolder": "server",
-"$.core.notify.postman": "services",
-"$.web.hub.internal": "http://onlyoffice-socket:"+SERVICE_PORT+"/",
-"$.files.docservice.url.portal": APP_URL_PORTAL,
-"$.files.docservice.url.public": DOCUMENT_SERVER_URL_PUBLIC,
-"$.files.docservice.url.internal": DOCUMENT_SERVER_URL_INTERNAL,
-"$.files.docservice.secret.value":  DOCUMENT_SERVER_JWT_SECRET,
-"$.files.docservice.secret.header": DOCUMENT_SERVER_JWT_HEADER}
+    parametrsForUpdate = {
+    "$.ConnectionStrings.default.connectionString": "Server="+MYSQL_HOST+";Port=3306;Database="+MYSQL_DATABASE+";User ID="+MYSQL_USER+";Password="+MYSQL_PASSWORD+";Pooling=true;Character Set=utf8;AutoEnlist=false;SSL Mode=none;ConnectionReset=false",
+    "$.core.base-domain": APP_CORE_BASE_DOMAIN,
+    "$.core.machinekey": APP_CORE_MACHINEKEY,
+    "$.core.products.subfolder": "server",
+    "$.core.notify.postman": "services",
+    "$.web.hub.internal": "http://onlyoffice-socket:"+SERVICE_PORT+"/",
+    "$.files.docservice.url.portal": APP_URL_PORTAL,
+    "$.files.docservice.url.public": DOCUMENT_SERVER_URL_PUBLIC,
+    "$.files.docservice.url.internal": DOCUMENT_SERVER_URL_INTERNAL,
+    "$.files.docservice.secret.value":  DOCUMENT_SERVER_JWT_SECRET,
+    "$.files.docservice.secret.header": DOCUMENT_SERVER_JWT_HEADER}
 
-for key, value in parametrsForUpdate.items():
-    updateJsonData(jsonData, key, value)
+    for key, value in parametrsForUpdate.items():
+        updateJsonData(jsonData, key, value)
 
-if INSTALLATION_TYPE == "ENTERPRISE":
-    updateJsonData(jsonData, "$.license.file.path", "/app/onlyoffice/data/license.lic")
+    if INSTALLATION_TYPE == "ENTERPRISE":
+        updateJsonData(jsonData, "$.license.file.path", "/app/onlyoffice/data/license.lic")
 
-ip_address = netifaces.ifaddresses('eth0').get(netifaces.AF_INET)[0].get('addr')
-netmask = netifaces.ifaddresses('eth0').get(netifaces.AF_INET)[0].get('netmask')
-ip_address_netmask = '%s/%s' % (ip_address, netmask)
-interface_cidr = IPNetwork(ip_address_netmask)
-knownNetwork = [str(interface_cidr)]
-knownProxies = ["127.0.0.1"]
+    ip_address = netifaces.ifaddresses('eth0').get(netifaces.AF_INET)[0].get('addr')
+    netmask = netifaces.ifaddresses('eth0').get(netifaces.AF_INET)[0].get('netmask')
+    ip_address_netmask = '%s/%s' % (ip_address, netmask)
+    interface_cidr = IPNetwork(ip_address_netmask)
+    knownNetwork = [str(interface_cidr)]
+    knownProxies = ["127.0.0.1"]
 
-if APP_KNOWN_NETWORKS:
-    knownNetwork= knownNetwork + [x.strip() for x in APP_KNOWN_NETWORKS.split(',')]
+    if APP_KNOWN_NETWORKS:
+        knownNetwork= knownNetwork + [x.strip() for x in APP_KNOWN_NETWORKS.split(',')]
 
-if APP_KNOWN_PROXIES:
-    knownNetwork= knownNetwork + [x.strip() for x in APP_KNOWN_PROXIES.split(',')]
+    if APP_KNOWN_PROXIES:
+        knownNetwork= knownNetwork + [x.strip() for x in APP_KNOWN_PROXIES.split(',')]
 
-updateJsonData(jsonData,"$.core.hosting.forwardedHeadersOptions.knownNetworks", knownNetwork)
-updateJsonData(jsonData,"$.core.hosting.forwardedHeadersOptions.knownProxies", knownProxies)
+    updateJsonData(jsonData,"$.core.hosting.forwardedHeadersOptions.knownNetworks", knownNetwork)
+    updateJsonData(jsonData,"$.core.hosting.forwardedHeadersOptions.knownProxies", knownProxies)
 
-writeJsonFile(filePath, jsonData)
+    writeJsonFile(filePath, jsonData)
 
-filePath = "/app/onlyoffice/config/apisystem.json"
-jsonData = openJsonFile(filePath)
-updateJsonData(jsonData, "$.ConnectionStrings.default.connectionString", "Server="+ MYSQL_HOST +";Port=3306;Database="+ MYSQL_DATABASE +";User ID="+ MYSQL_USER +";Password="+ MYSQL_PASSWORD +";Pooling=true;Character Set=utf8;AutoEnlist=false;SSL Mode=none;ConnectionReset=false",)
-updateJsonData(jsonData,"$.core.base-domain", APP_CORE_BASE_DOMAIN)
-updateJsonData(jsonData,"$.core.machinekey", APP_CORE_MACHINEKEY)
-writeJsonFile(filePath, jsonData)
+    filePath = "/app/onlyoffice/config/apisystem.json"
+    jsonData = openJsonFile(filePath)
+    updateJsonData(jsonData, "$.ConnectionStrings.default.connectionString", "Server="+ MYSQL_HOST +";Port=3306;Database="+ MYSQL_DATABASE +";User ID="+ MYSQL_USER +";Password="+ MYSQL_PASSWORD +";Pooling=true;Character Set=utf8;AutoEnlist=false;SSL Mode=none;ConnectionReset=false",)
+    updateJsonData(jsonData,"$.core.base-domain", APP_CORE_BASE_DOMAIN)
+    updateJsonData(jsonData,"$.core.machinekey", APP_CORE_MACHINEKEY)
+    writeJsonFile(filePath, jsonData)
 
-filePath = "/app/onlyoffice/config/elastic.json"
-jsonData = openJsonFile(filePath)
-jsonData["elastic"]["Scheme"] = ELK_SHEME
-jsonData["elastic"]["Host"] = ELK_HOST
-jsonData["elastic"]["Port"] = ELK_PORT
-jsonData["elastic"]["Threads"] = ELK_THREADS
-writeJsonFile(filePath, jsonData)
+    filePath = "/app/onlyoffice/config/elastic.json"
+    jsonData = openJsonFile(filePath)
+    jsonData["elastic"]["Scheme"] = ELK_SHEME
+    jsonData["elastic"]["Host"] = ELK_HOST
+    jsonData["elastic"]["Port"] = ELK_PORT
+    jsonData["elastic"]["Threads"] = ELK_THREADS
+    writeJsonFile(filePath, jsonData)
 
-filePath = "/app/onlyoffice/config/kafka.json"
-jsonData = openJsonFile(filePath)
-jsonData.update({"kafka": {"BootstrapServers": KAFKA_HOST}})
-writeJsonFile(filePath, jsonData)
+    filePath = "/app/onlyoffice/config/kafka.json"
+    jsonData = openJsonFile(filePath)
+    jsonData.update({"kafka": {"BootstrapServers": KAFKA_HOST}})
+    writeJsonFile(filePath, jsonData)
 
-filePath = "/app/onlyoffice/config/socket.json"
-jsonData = openJsonFile(filePath)
-updateJsonData(jsonData,"$.socket.port", SERVICE_PORT)
-writeJsonFile(filePath, jsonData)
+    filePath = "/app/onlyoffice/config/socket.json"
+    jsonData = openJsonFile(filePath)
+    updateJsonData(jsonData,"$.socket.port", SERVICE_PORT)
+    writeJsonFile(filePath, jsonData)
 
-filePath = "/app/onlyoffice/config/ssoauth.json"
-jsonData = openJsonFile(filePath)
-updateJsonData(jsonData,"$.ssoauth.port", SERVICE_PORT)
-writeJsonFile(filePath, jsonData)
+    filePath = "/app/onlyoffice/config/ssoauth.json"
+    jsonData = openJsonFile(filePath)
+    updateJsonData(jsonData,"$.ssoauth.port", SERVICE_PORT)
+    writeJsonFile(filePath, jsonData)
 
-filePath = "/app/onlyoffice/config/rabbitmq.json"
-jsonData = openJsonFile(filePath)
-updateJsonData(jsonData,"$.RabbitMQ.Hostname", RABBIT_HOST)
-updateJsonData(jsonData,"$.RabbitMQ.UserName", RABBIT_USER_NAME)
-updateJsonData(jsonData, "$.RabbitMQ.Password", RABBIT_PASSWORD)
-updateJsonData(jsonData, "$.RabbitMQ.Port", RABBIT_PORT)
-updateJsonData(jsonData, "$.RabbitMQ.VirtualHost", RABBIT_VIRTUAL_HOST)
-jsonData["RabbitMQ"].update(RABBIT_URI) if RABBIT_URI is not None else None
-writeJsonFile(filePath, jsonData)
+    filePath = "/app/onlyoffice/config/rabbitmq.json"
+    jsonData = openJsonFile(filePath)
+    updateJsonData(jsonData,"$.RabbitMQ.Hostname", RABBIT_HOST)
+    updateJsonData(jsonData,"$.RabbitMQ.UserName", RABBIT_USER_NAME)
+    updateJsonData(jsonData, "$.RabbitMQ.Password", RABBIT_PASSWORD)
+    updateJsonData(jsonData, "$.RabbitMQ.Port", RABBIT_PORT)
+    updateJsonData(jsonData, "$.RabbitMQ.VirtualHost", RABBIT_VIRTUAL_HOST)
+    jsonData["RabbitMQ"].update(RABBIT_URI) if RABBIT_URI is not None else None
+    writeJsonFile(filePath, jsonData)
 
-filePath = "/app/onlyoffice/config/redis.json"
-jsonData = openJsonFile(filePath)
-updateJsonData(jsonData,"$.Redis.Hosts.[0].Host", REDIS_HOST)
-updateJsonData(jsonData,"$.Redis.Hosts.[0].Port", REDIS_PORT)
-jsonData["Redis"].update(REDIS_USER_NAME) if REDIS_USER_NAME is not None else None
-jsonData["Redis"].update(REDIS_PASSWORD) if REDIS_PASSWORD is not None else None
-writeJsonFile(filePath, jsonData)
+    filePath = "/app/onlyoffice/config/redis.json"
+    jsonData = openJsonFile(filePath)
+    updateJsonData(jsonData,"$.Redis.Hosts.[0].Host", REDIS_HOST)
+    updateJsonData(jsonData,"$.Redis.Hosts.[0].Port", REDIS_PORT)
+    jsonData["Redis"].update(REDIS_USER_NAME) if REDIS_USER_NAME is not None else None
+    jsonData["Redis"].update(REDIS_PASSWORD) if REDIS_PASSWORD is not None else None
+    writeJsonFile(filePath, jsonData)
 
-run = RunServices(SERVICE_PORT, PATH_TO_CONF)
-run.RunService(RUN_FILE, ENV_EXTENSION, LOG_FILE)
+    run = RunServices(SERVICE_PORT, PATH_TO_CONF)
+    run.RunService(RUN_FILE, ENV_EXTENSION, LOG_FILE)
+
+
+args = sys.argv[1:]
+
+if len(args) == 0:
+    print("Необходимо указать имя функции в аргументах командной строки")
+else:
+    function_name = args[0]
+    if function_name == 'main':
+        main()
+    else:
+        print("Неверное имя функции")
+
